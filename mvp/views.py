920ba20ad3feec78dab6aaac6d2563260212ac9a
lahpm092@gmail.com <lahpmx@lahpmxs-MacBook-Pro.local>
2020-01-04 15:06:10 -0600

    second

--- mvp/views.py
+++ mvp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .forms import NameForm
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    if request.method == 'POST':
+        name = request.POST["your_name"]
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return render(request, 'mvp/thanks.html', {'name': name})
+
+    else:
+        form = NameForm()
+
+
+    return render(request,'mvp/index.html', {'form': form})
